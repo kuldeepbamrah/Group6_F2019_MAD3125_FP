5b528fb4171a6e76b48485a74f2fa4a04350e4fd
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-12-06 11:36:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/group6_f2019_mad3125_fp/ui/send/SendViewModel.java
@@ -0,0 +1,19 @@
+package com.example.group6_f2019_mad3125_fp.ui.send;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SendViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SendViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is send fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
