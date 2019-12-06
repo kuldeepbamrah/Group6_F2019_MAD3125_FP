9b08f01dfd892ce1d85b115e79998b140f2f627a
rizul <rizulgoyal23@gmail.com>
2019-12-06 17:49:41 -0500

    jkdsnf

--- app/src/main/java/com/example/group6_f2019_mad3125_fp/Activities/MainActivity.java
+++ app/src/main/java/com/example/group6_f2019_mad3125_fp/Activities/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
