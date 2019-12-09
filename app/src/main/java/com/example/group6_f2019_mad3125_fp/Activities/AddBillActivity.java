ac31217d553e6806f627e6d1401bf2bf7508b9d4
rizul <rizulgoyal23@gmail.com>
2019-12-09 18:13:07 -0500

    add bill activity

---
+++ app/src/main/java/com/example/group6_f2019_mad3125_fp/Activities/AddBillActivity.java
@@ -0,0 +1,16 @@
+package com.example.group6_f2019_mad3125_fp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.group6_f2019_mad3125_fp.R;
+
+public class AddBillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_bill);
+    }
+}
