bf6b0575fa7c9f2c98db6f489cadb5f077851775
rizul <rizulgoyal23@gmail.com>
2019-12-09 16:11:46 -0500

    new file for splash activity

---
+++ app/src/main/java/com/example/group6_f2019_mad3125_fp/Activities/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.group6_f2019_mad3125_fp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
