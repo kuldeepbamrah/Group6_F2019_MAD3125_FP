faa9c0cfda5f2596d72853b472b607bd7ee0bc48
rizul <rizulgoyal23@gmail.com>
2019-12-09 16:17:51 -0500

    splash background setting photo

--- app/src/main/java/com/example/group6_f2019_mad3125_fp/Activities/SplashActivity.java
+++ app/src/main/java/com/example/group6_f2019_mad3125_fp/Activities/SplashActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.group6_f2019_mad3125_fp.R;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
     }
 }
