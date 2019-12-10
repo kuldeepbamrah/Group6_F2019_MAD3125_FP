899aef3013e9ee3e427660b2cc94fff037814c7c
rizul <rizulgoyal23@gmail.com>
2019-12-10 15:13:30 -0500

    vehicle update on restart callback

--- app/src/main/java/com/example/group6_f2019_mad3125_fp/Activities/EmployeeDetailActivity.java
+++ app/src/main/java/com/example/group6_f2019_mad3125_fp/Activities/EmployeeDetailActivity.java
@@ -33,6 +33,40 @@ public class EmployeeDetailActivity extends AppCompatActivity {
     LinearLayout intern,fulltime,parttime;
 
 
+    @Override
+    protected void onRestart() {
+
+        Employee myemp = getIntent().getParcelableExtra("empobject");
+
+
+        List<Vehicle> vehicles = myemp.getVehicle();
+
+            RecyclerView recyclerView = findViewById(R.id.recycler_vehicle);
+            final VehicleDataAdapter vehicleDataAdapter = new VehicleDataAdapter(this);
+          vehicleDataAdapter.setMyaaraylist(vehicles);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setAdapter(vehicleDataAdapter);
+
+            final EmployeeDB uData = EmployeeDB.getInstance(this);
+
+
+            uData.daoObjct().getCurrentUserDetails(myemp.getId()).observe(this, new Observer<Employee>() {
+                @Override
+                public void onChanged(@Nullable Employee employee) {
+                    List<Vehicle> bills = employee.getVehicle();
+                    vehicleDataAdapter.setMyaaraylist(bills);
+                    vehicleDataAdapter.notifyDataSetChanged();
+                }
+            });
+
+        super.onRestart();
+
+
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -130,14 +164,14 @@ public class EmployeeDetailActivity extends AppCompatActivity {
             final EmployeeDB uData = EmployeeDB.getInstance(this);
 
 
-            uData.daoObjct().getCurrentUserDetails(myemp.getId()).observe(this, new Observer<Employee>() {
-                @Override
-                public void onChanged(@Nullable Employee employee) {
-                    List<Vehicle> bills = employee.getVehicle();
-                    vehicleDataAdapter.setMyaaraylist(bills);
-                    vehicleDataAdapter.notifyDataSetChanged();
-                }
-            });
+//            uData.daoObjct().getCurrentUserDetails(myemp.getId()).observe(this, new Observer<Employee>() {
+//                @Override
+//                public void onChanged(@Nullable Employee employee) {
+//                    List<Vehicle> bills = employee.getVehicle();
+//                    vehicleDataAdapter.setMyaaraylist(bills);
+//                    vehicleDataAdapter.notifyDataSetChanged();
+//                }
+//            });
 
         }
 
